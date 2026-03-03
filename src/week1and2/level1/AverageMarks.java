03ac6a3cccceaa4ff6e02f74d9e52fe9d17347f1
DakshSwami <dakshswami212@gmail.com>
2026-03-03 11:21:47 +0530

    Coded Level 2 Problems

--- src/week1and2/level1/AverageMarks.java
+++
@@ -1,8 +0,0 @@
-class AverageMarks {
-    public static void main(String[] args) {
-        int maths = 94, physics = 95, chemistry = 96;
-        int totalMarks = maths + physics + chemistry;
-        double averagePercent = (double) totalMarks / 3;
-        System.out.println("Sam's average mark in PCM is " + averagePercent);
-    }
-}
